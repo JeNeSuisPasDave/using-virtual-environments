ec7df6b7284db5b7c75be52cb700efa354a75d7d
Dave Hein <jenesuispasdave@gmail.com>
2016-02-15 08:30:11 -0600

    Added provisioning scripts for venv27
    Added provisioning scripts for a Python 2.7 virtual environment.

---
+++ python/make-venv.sh
@@ -0,0 +1,105 @@
+#! /bin/bash
+#
+# This script assumes that Python 2.7 is installed via Mac Ports.
+# If Python 2.7 is installed some other way, then the script will
+# need some adjustment.
+#
+
+VENV_DIRNAME_=venv27
+SAVE_VENV_DIRNAME_=""
+PYTHON_VERSION_=2.7
+PYTHON_INSTALLROOT_=/opt/local/Library/Frameworks/Python.framework/Versions
+PYTHON_INSTALLBASE_=${PYTHON_INSTALLROOT_}/${PYTHON_VERSION_}
+
+# Check whether Python 2.7 is installed via Mac Ports
+#
+if ! [ -d "${PYTHON_INSTALLBASE_}" ]; then
+  echo "ERROR: Python 2.7 is not installed."
+  echo "       Expected '${PYTHON_INSTALLBASE_}'"
+  echo "Install using 'sudo port install python27'."
+  exit 2
+fi
+
+# Check whether we are running in a python virtual environment
+#
+export VENV_RUNNING=`env | grep VIRTUAL_ENV | wc -l | tr -d [[:space:]]`
+#echo "VENV_RUNNING: ${VENV_RUNNING}"
+if [ 0 != ${VENV_RUNNING} ]; then
+  echo "ERROR: Python virtual environment already running"
+  echo
+  echo "Try 'deactivate' to stop the virtual environment, and"
+  echo "then try './make-venv27.sh' again."
+  echo
+  exit 4
+fi
+
+# Check whether the virtual environment directory already exists
+#
+if [ -e "./${VENV_DIRNAME_}" ]; then
+  echo "WARNING: virtual environment directory ./${VENV_DIRNAME_} already exists."
+  read -p "Do you want to delete and replace it? [y/N]" -n 1 -r
+  echo    # (optional) move to a new line
+  if [[ ! $REPLY =~ ^[Yy]$ ]]
+  then
+    echo "No action taken".
+    exit 6
+  fi
+  # Rename the existing directory
+  #
+  SAVE_VENV_DIRNAME_="${VENV_DIRNAME_}_bkup"
+  mv "./${VENV_DIRNAME_}" "./$SAVE_VENV_DIRNAME_"
+fi
+
+# Create the virtual environment directory (the deployment folder)
+#
+"${PYTHON_INSTALLBASE_}/bin/virtualenv" "${VENV_DIRNAME_}"
+if [ -d "./${VENV_DIRNAME_}" ]; then
+  if ! [ -z "${SAVE_VENV_DIRNAME_}" ]; then
+    rm -rf "./${SAVE_VENV_DIRNAME_}"
+  fi
+else
+  echo "ERROR: virtual environment not created."
+  if ! [ -z "${SAVE_VENV_DIRNAME_}" ]; then
+    mv "./${SAVE_VENV_DIRNAME_}" "./${VENV_DIRNAME_}"
+  fi
+  exit 8
+fi
+
+# Activate the virtual environment
+#
+. "${VENV_DIRNAME_}/bin/activate"
+# Check whether we are running in a python virtual environment
+#
+export VENV_RUNNING=`env | grep VIRTUAL_ENV | wc -l | tr -d [[:space:]]`
+#echo "VENV_RUNNING: ${VENV_RUNNING}"
+if [ 0 == ${VENV_RUNNING} ]; then
+  echo "ERROR: Python virtual environment was not activated; not running"
+  exit 10
+fi
+
+# Check whether we are running Python 2.7
+#
+export PYVER_=`python --version 2>&1 | grep "^Python 2\.7\." | wc -l | tr -d [[:space:]]`
+if [ 0 == ${PYVER_} ]; then
+  echo "ERROR: Python 2.7 is required. Found "`python --version`"."
+  echo
+  deactivate
+  exit 12
+fi
+
+# Update setuptools and pip
+#
+pip install --upgrade -q setuptools
+pip install --upgrade -q pip
+
+# deactivate the virtual environment
+#
+deactivate
+
+# Done!
+#
+echo ""
+echo "OK. Virtual environment for Python 2.7 is created."
+echo "Use source command '. ${VENV_DIRNAME_}/bin/activate' to start;"
+echo "Use command 'deactivate' to stop."
+echo ""
---
+++ python/provision-venv.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+#
+# These scripts assumes that Python 2.7 is installed via Mac Ports.
+# If Python 2.7 is installed some other way, then the scripts will
+# need some adjustment.
+#
+
+SCRIPTNAME_="$0"
+
+# Get the directory holding this script
+# (method from: http://stackoverflow.com/a/12694189/1392864)
+#
+SCRIPTDIR_="${BASH_SOURCE%/*}"
+if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
+
+# Pick up the verification functions
+#
+. "${SCRIPTDIR_}/check-functions.src"
+
+# Check that we are running in a Python 2.7 virtual environment
+#
+. "${SCRIPTDIR_}/check-active-venv.src"
+
+# Install required packages
+#
+pip install -q -r requirements-venv.txt
+
+# Check that we have the needed packages
+#
+. "${SCRIPTDIR_}/check-dependencies.src"
+
+# Done!
+#
+echo ""
+echo -n "OK. Provisioned required packages for project in "
+echo "Python 2.7 virtual environment."
+echo ""
