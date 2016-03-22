de841eec4f20975cc8cb6c802a7fbbf77897005e
Dave Hein <jenesuispasdave@gmail.com>
2016-03-22 12:29:08 -0500

    Working through venv make script ...
    Checking whether the necessary version of Ruby is installed in rbenv.

--- ruby/make-venv.sh
+++ ruby/make-venv.sh
@@ -17,34 +17,6 @@ if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
 #
 . "${SCRIPTDIR_}/check-functions.src"
 
-RBLCL_DIRNAME_=.rblcl
-SAVE_RBLCL_DIRNAME_=""
-RUBY_VERSION_=2.0.0p481
-RUBY_RBENV_VERSION_=2.0.0-p481
-RBENV_VERSION_=0.4.0
-
-parse_version "${RUBY_VERSION_}"
-if (( 0 != $? )); then
-  echo "ERROR: failed to parse RUBY_VERSION_"
-  exit 2
-fi
-VSN_PARTS_=( ${parse_version[@]} )
-echo "MAJOR: ${VSN_PARTS_[0]}"
-echo "MINOR: ${VSN_PARTS_[1]}"
-echo "PATCH: ${VSN_PARTS_[2]}"
-echo "  SUB: ${VSN_PARTS_[3]}"
-
-parse_version "${RUBY_RBENV_VERSION_}"
-if (( 0 != $? )); then
-  echo "ERROR: failed to parse RUBY_RBENV_VERSION_"
-  exit 2
-fi
-VSN_PARTS_=( ${parse_version[@]} )
-echo "MAJOR: ${VSN_PARTS_[0]}"
-echo "MINOR: ${VSN_PARTS_[1]}"
-echo "PATCH: ${VSN_PARTS_[2]}"
-echo "  SUB: ${VSN_PARTS_[3]}"
-
 # Check whether rbenv is installed
 #
 `which -s rbenv`
@@ -54,6 +26,57 @@ if (( 0 != RBENV_IN_PATH_ )); then
   echo "Install using 'sudo port install rbenv'."
   exit 2
 fi
+
+# Determine the version of Ruby we want
+#
+RUBY_VERSION_="2.0.0p481"
+parse_version "${RUBY_VERSION_}"
+if (( 0 != $? )); then
+  echo "ERROR: failed to parse RUBY_VERSION_"
+  exit 2
+fi
+RUBY_VERSION_PARTS_=( ${parse_version[@]} )
+
+# Check if the Ruby version we want is available
+#
+FOUND_=0
+RBENV_VERSIONS_=( `rbenv versions --bare` )
+for (( i = 0 ; i < ${#RBENV_VERSIONS_[@]} ; i++ ))
+do
+  VSN_="${RBENV_VERSIONS_[$i]}"
+  parse_version "${VSN_}"
+  if (( 0 != $? )); then
+    echo "ERROR: failed to parse VSN_"
+    exit 2
+  fi
+  VSN_PARTS_=( ${parse_version[@]} )
+  if [[ "${RUBY_VERSION_PARTS_[@]}" == "${VSN_PARTS_[@]}" ]]; then
+    FOUND_=1
+    break
+  fi
+done
+
+# If not found, install it
+#
+if (( 0 == $FOUND_ )); then
+  echo "ERROR: need to install with command 'rbenv install $RBENV_RUBY_VERSION_'"
+  exit 2
+fi
+
+# Set the local Ruby version
+#
+`rbenv local $RBENV_RUBY_VERSION_`
+
+# Check the local Ruby version
+#
+`rbenv version`
+
+# RBLCL_DIRNAME_=.rblcl
+# SAVE_RBLCL_DIRNAME_=""
+# RUBY_VERSION_=2.0.0p481
+# RUBY_RBENV_VERSION_=2.0.0-p481
+# RBENV_VERSION_=0.4.0
+
 
 # Check which version of ruby is active, per rbenv
 #
